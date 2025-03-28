091c8073671ed6e2583967ca3b4c022fd9a3dc35
Eva <evauwu@riseup.net>
2025-03-28 15:33:26 +0100

    client: remove unused function

--- client/js/main.js
+++ client/js/main.js
@@ -46,8 +46,6 @@ if (!settings.get().darkTheme) {
     document.body.classList.remove("darktheme");
 }
 
-const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
-
 window.RufflePlayer = window.RufflePlayer || {};
 window.RufflePlayer.config = {
     "polyfills": true,

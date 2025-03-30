48095611f5a45123d4a6a06f315e840380a188d0
Eva <evauwu@riseup.net>
2025-03-30 05:31:32 +0200

    server/posts: reorder content upload code, tolerate duplicate checksums cf. #539

--- server/szurubooru/func/posts.py
+++ server/szurubooru/func/posts.py
@@ -679,14 +679,12 @@ def update_post_content(post: model.Post, content: Optional[bytes]) -> None:
             "Unhandled file type: %r" % post.mime_type
         )
 
-    post.checksum = util.get_sha1(content)
-    post.checksum_md5 = util.get_md5(content)
-    post.image_key = auth.create_password()
+    checksum = util.get_sha1(content)
     other_post = (
         db.session.query(model.Post)
-        .filter(model.Post.checksum == post.checksum)
+        .filter(model.Post.checksum == checksum)
         .filter(model.Post.post_id != post.post_id)
-        .one_or_none()
+        .first()
     )
     if (
         other_post
@@ -695,11 +693,6 @@ def update_post_content(post: model.Post, content: Optional[bytes]) -> None:
     ):
         raise PostAlreadyUploadedError(other_post)
 
-    if update_signature:
-        purge_post_signature(post)
-        post.signature = generate_post_signature(post, content)
-
-    post.file_size = len(content)
     try:
         image = images.Image(content)
         post.canvas_width = image.width
@@ -721,6 +714,16 @@ def update_post_content(post: model.Post, content: Optional[bytes]) -> None:
         else:
             post.canvas_width = None
             post.canvas_height = None
+
+    post.checksum = checksum
+    post.checksum_md5 = util.get_md5(content)
+    post.image_key = auth.create_password()
+    post.file_size = len(content)
+
+    if update_signature:
+        purge_post_signature(post)
+        post.signature = generate_post_signature(post, content)
+
     setattr(post, "__content", content)
 
 
@@ -997,7 +1000,7 @@ def search_by_image_exact(image_content: bytes) -> Optional[model.Post]:
     return (
         db.session.query(model.Post)
         .filter(model.Post.checksum == checksum)
-        .one_or_none()
+        .first()
     )
 
 
